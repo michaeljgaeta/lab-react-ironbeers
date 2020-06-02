241ecccc305d11b2a67938920e5738a23ef1c848
michaeljgaeta <michaeljgaeta@gmail.com>
2020-06-02 12:43:47 +0100

    iteration 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+import Home from './../src/views/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <>
+          <img src="./images/all-beers.png"></img>
+          <h2>All Beers</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis culpa
+            repudiandae dicta, a iste quas dignissimos amet esse labore quasi
+            vitae nihil facilis dolor totam, quam aliquid illum veritatis
+            debitis!
+          </p>
+        </>
+        <>
+          <img src="./images/random-beers.png"></img>
+          <h2>Random Beer</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis culpa
+            repudiandae dicta, a iste quas dignissimos amet esse labore quasi
+            vitae nihil facilis dolor totam, quam aliquid illum veritatis
+            debitis!
+          </p>
+        </>
+        <>
+          <img src="./images/new-beer.png"></img>
+          <h2>New Beer</h2>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis culpa
+            repudiandae dicta, a iste quas dignissimos amet esse labore quasi
+            vitae nihil facilis dolor totam, quam aliquid illum veritatis
+            debitis!
+          </p>
+        </>
+      </div>
+    );
+  }
+}
+
+export default Home;
